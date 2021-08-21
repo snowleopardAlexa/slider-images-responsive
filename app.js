21895531fe263e04205214e821b57ddcb0138043
snowleopardAlexa <alexaslomski@gmail.com>
2021-08-21 16:13:26 -0400

    add jquery

---
+++ app.js
@@ -0,0 +1,7 @@
+const carouselSlider = document.querySelector('.carousel-slider');
+const carouselImages = document.querySelectorAll('.carousel-slider img');
+
+// buttons
+const prevBtn = document.querySelector('#prevBtn');
+const nextBtn = document.querySelector('#nextBtn');
+
