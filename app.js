cf178594685c780ae6e0600e8e5c25a05e183472
snowleopardAlexa <alexaslomski@gmail.com>
2021-08-21 16:23:11 -0400

    add prev btn

--- app.js
+++ app.js
@@ -12,3 +12,17 @@ const size = carouselImages[0].clientWidth;
 // move one photo ahead
 carouselSlider.style.transform = 'translateX(' + (-size * counter) + 'px)';
 
+// button listeners
+// next button 
+nextBtn.addEventListener('click', () => {
+  carouselSlider.style.transition = "transform 0.4s ease-in-out"
+  counter++;
+  carouselSlider.style.transform = 'translateX(' + (-size * counter) + 'px)';
+});
+
+// prev button 
+prevBtn.addEventListener('click', () => {
+    carouselSlider.style.transition = "transform 0.4s ease-in-out"
+    counter--;
+    carouselSlider.style.transform = 'translateX(' + (-size * counter) + 'px)';
+});
