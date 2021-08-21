9909e8a73af7df48741c98890e38497969033b2c
snowleopardAlexa <alexaslomski@gmail.com>
2021-08-21 16:16:33 -0400

    add counter and move one photo ahead

--- app.js
+++ app.js
@@ -5,3 +5,10 @@ const carouselImages = document.querySelectorAll('.carousel-slider img');
 const prevBtn = document.querySelector('#prevBtn');
 const nextBtn = document.querySelector('#nextBtn');
 
+// counter
+let counter = 1;
+const size = carouselImages[0].clientWidth;
+
+// move one photo ahead
+carouselSlider.style.transform = 'translateX(' + (-size * counter) + 'px)';
+
